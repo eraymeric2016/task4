ea64f8eb2bba304d5f120d38f651199cf649d9d1
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:13:31 +0300

    Add files via upload

---
+++ task4.java
@@ -0,0 +1,53 @@
+package week_03;
+
+import java.util.Scanner;
+
+/*
+Write a Java program to convert temperature from Fahrenheit to Celsius degree
+        - Input a degree in Fahrenheit: 212
+
+                            Formula :         C = (5(F-32))/9
+
+        - Expected Output:
+                            212.0 degree Fahrenheit is equal to 100.0 in Celsius
+ */
+public class task4 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+        double fah=212;
+        double cel=(5*(fah-32))/9;
+        //System.out.println(30.0/5);
+        System.out.println(cel);
+        ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
